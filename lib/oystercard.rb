34fba4ebffe3f7b793f9855a32edb0e06dff76d7
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-01-13 15:43:06 +0000

    journeys method created

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,32 +1,38 @@
 class Oystercard
 
-  MAXIMUM_BALANCE = 90 
+  MAXIMUM_BALANCE = 90
 
   MINIMUM_CHARGE = 1
 
-  attr_reader :balance, :in_travel, :entry_station
+  attr_reader :balance, :in_travel, :entry_station,
+    :exit_station, :journeys
 
   def initialize
     @balance = 0
     @entry_station = nil
+    @exit_station = nil
+    @journeys = {}
   end
 
   def top_up(amount)
     fail 'Maximum balance of #{maximum_balance} exceeded' if amount + balance > MAXIMUM_BALANCE
     @balance += amount
-  end 
+  end
 
   def touch_in (station_name)
     fail "No money" if balance < 1
+    @journeys[:entry] = station_name
     @entry_station = station_name
-  end 
+  end
 
   def in_journey?
-  @entry_station != nil
+    @entry_station != nil
   end
-  
-  def touch_out
+
+  def touch_out(station_name)
     deduct(MINIMUM_CHARGE)
+    @exit_station = station_name
+    @journeys[:exit] = station_name
     @entry_station = nil
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,76 +2,84 @@ require 'oystercard'
 
 describe Oystercard do
 let(:station_name) {double :Station_name }
+let(:station2_name) {double :Station_name }
 
-describe "#balance" do
-  it "should check a card has a balance" do
-    expect(subject.balance).to eq(0)
+  describe "#balance" do
+    it "should check a card has a balance" do
+      expect(subject.balance).to eq(0)
+    end
   end
-end
 
   describe "#top_up" do
-  it "should top up balance" do
-    expect{ subject.top_up(20) }.to change{ subject.balance }.by 20
+    it "should top up balance" do
+      expect{ subject.top_up(20) }.to change{ subject.balance }.by 20
+    end
+
+    it 'raises an error if the maximum balance is exceeded' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(maximum_balance)
+      expect{ subject.top_up 1 }.to raise_error 'Maximum balance of #{maximum_balance} exceeded'
+    end
   end
 
-  it 'raises an error if the maximum balance is exceeded' do
-    maximum_balance = Oystercard::MAXIMUM_BALANCE 
-    subject.top_up(maximum_balance)
-    expect{ subject.top_up 1 }.to raise_error 'Maximum balance of #{maximum_balance} exceeded'
-  end 
-end
 
-
-describe "#touch_in" do
-  it 'should touch in oystercard' do
-    subject.top_up(5)
-    expect(subject.touch_in(station_name)).to eq (station_name)
-  end 
-  it 'should raise an error if balance is less than 1' do
-    expect { subject.touch_in(station_name) }.to raise_error "No money"
+  describe "#touch_in" do
+    it 'should touch in oystercard' do
+      subject.top_up(5)
+      expect(subject.touch_in(station_name)).to eq (station_name)
+    end
+    it 'should raise an error if balance is less than 1' do
+      expect { subject.touch_in(station_name) }.to raise_error "No money"
+    end
   end
-end
 
-describe "#in_journey" do
-
-  it 'should check if oystercard is in_journey?' do
-    subject.top_up(5)
-    subject.touch_in(station_name)
-    expect(subject.in_journey?).to eq(true)
-  end 
-end
+  describe "#in_journey" do
+    it 'should check if oystercard is in_journey?' do
+      subject.top_up(5)
+      subject.touch_in(station_name)
+      expect(subject.in_journey?).to eq(true)
+    end
+  end
 
   describe "#touch_out" do
   it 'should touch out oystercard' do
     subject.top_up(5)
     subject.touch_in(station_name)
-    expect(subject.touch_out).to eq (nil)
-  end 
+    expect(subject.touch_out(station2_name)).to eq (nil)
+  end
   it 'should take off minimum cost when touching out' do
     subject.top_up(5)
     subject.touch_in(station_name)
-    expect { subject.touch_out }.to change { subject.balance }.by(-Oystercard::MINIMUM_CHARGE)
+    expect { subject.touch_out(station2_name) }.to change { subject.balance }.by(-Oystercard::MINIMUM_CHARGE)
   end
 end
 
-describe "#entry_station" do
-  
-  it 'oystercard should return station value of nil' do
-    subject.top_up(5)
-    subject.touch_in(station_name)
-    subject.touch_out
-    expect(subject.entry_station).to eq (nil)
+  describe "#entry_station" do
+    it 'oystercard should return station value of nil' do
+      subject.top_up(5)
+      subject.touch_in(station_name)
+      subject.touch_out(station2_name)
+      expect(subject.entry_station).to eq (nil)
+    end
+    it 'should state the name of station you have travelled from' do
+      subject.top_up(5)
+      subject.touch_in(station_name)
+      expect(subject.entry_station).to eq(station_name)
+    end
   end
-  it 'should state the name of station you have travelled from' do
-    subject.top_up(5)
-    subject.touch_in(station_name)
-    expect(subject.entry_station).to eq(station_name)
+
+  describe "#exit_station" do
+    it 'will return the station_name on exit' do
+      subject.top_up(5)
+      subject.touch_in(station_name)
+      subject.touch_out(station2_name)
+      expect(subject.exit_station).to eq(station2_name)
+    end
   end
-end
 
- 
-
-  
-
-  
+  describe "#journeys" do
+    it 'will return an empty hash by default' do
+      expect(subject.journeys).to eq({})
+    end
+  end
 end
